632b3880bbfaf981704831e5b2b9fcc58a615ca5
Ana Mitsutani <ana.mitsutani@gmail.com>
2021-01-19 15:41:46 -0300

    added .env file and deleted useless file

--- hello.py
+++ hello.py
@@ -1,1 +1,17 @@
-print("Hellow ana")
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await client.send_message(message.channel, 'Done sleeping')
+
+client.run(os.getenv('TOKEN'))
